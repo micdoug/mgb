703b446509f7ebdb6c35f522d350ced4afd182e3
Michael Dougras da Silva <micdoug.silva@gmail.com>
2018-07-16 08:44:48 -0300

    Fix setup.py package adding

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(exclude=['mgb']),  # Required
+    packages=find_packages(),  # Required
 
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
